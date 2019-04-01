1dac16b796377c17b25d237886931edc062c4dd0
Maciej Kudas <maciejkudas@gmail.com>
2019-04-01 12:59:24 +0200

    feat(GuzzleCollector): Add Guzzle collector (#1)
    Add Guzzle requests collector.

---
+++ src/GuzzleCollector.php
@@ -0,0 +1,118 @@
+<?php
+declare(strict_types=1);
+
+namespace Miquido\RequestDataCollector\Collectors\GuzzleCollector;
+
+use Illuminate\Contracts\Container\Container;
+use Miquido\RequestDataCollector\Collectors\Contracts\ConfigurableInterface;
+use Miquido\RequestDataCollector\Collectors\Contracts\DataCollectorInterface;
+use Miquido\RequestDataCollector\Collectors\Contracts\ModifiesContainerInterface;
+use Miquido\RequestDataCollector\Traits\ConfigurableTrait;
+use Psr\Http\Message\RequestInterface;
+
+class GuzzleCollector implements DataCollectorInterface, ConfigurableInterface, ModifiesContainerInterface
+{
+    use ConfigurableTrait;
+
+    public const TYPE_BIND = 'bind';
+
+    public const TYPE_SINGLETON = 'singleton';
+
+    /**
+     * @var array
+     */
+    private $requests = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function register(Container $container): void
+    {
+        $container->singleton(self::class, function (): self {
+            return $this;
+        });
+
+        $abstractsMap = [];
+
+        foreach ($this->config['decorate']['abstracts'] as $abstract => ['type' => $type, 'create' => $create]) {
+            $hasAbstract = $container->has($abstract);
+
+            if (!$hasAbstract && !$create) {
+                continue;
+            }
+
+            $oldAbstract = \sprintf('request-data-collector.old-abstract.%s', $abstract);
+
+            if (!$hasAbstract) {
+                $container->{$type}($oldAbstract, $abstract);
+
+                $oldInstance = $container->make($oldAbstract);
+            } else {
+                $oldInstance = $container->make($abstract);
+
+                $container->{$type}($oldAbstract, function () use (&$oldInstance): object {
+                    return $oldInstance;
+                });
+            }
+
+            $abstractsMap[] = [
+                'abstract'    => $abstract,
+                'type'        => $type,
+                'oldInstance' => $oldInstance,
+            ];
+        }
+
+        foreach ($abstractsMap as ['abstract' => $abstract, 'type' => $type, 'oldInstance' => $instance]) {
+            $container->{$type}($abstract, function () use ($container, $instance): object {
+                return $container->make($this->config['decorate']['with'], [
+                    'client' => $instance,
+                ]);
+            });
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function collect(): array
+    {
+        return $this->requests;
+    }
+
+    /**
+     * @param string                             $via
+     * @param \Psr\Http\Message\RequestInterface $request
+     * @param array                              $options
+     * @param array                              $times
+     */
+    public function addRequest(string $via, RequestInterface $request, array $options, array $times): void
+    {
+        $this->requests[] = [
+            'via'     => $via,
+            'method'  => $request->getMethod(),
+            'uri'     => (string) $request->getUri(),
+            'headers' => $request->getHeaders(),
+            'options' => $options,
+            'times'   => $times,
+        ];
+    }
+
+    /**
+     * @param string                                $via
+     * @param string                                $method
+     * @param \Psr\Http\Message\UriInterface|string $uri
+     * @param array                                 $options
+     * @param array                                 $times
+     */
+    public function addRawRequest(string $via, string $method, $uri, array $options, array $times): void
+    {
+        $this->requests[] = [
+            'via'     => $via,
+            'method'  => $method,
+            'uri'     => (string) $uri,
+            'headers' => [],
+            'options' => $options,
+            'times'   => $times,
+        ];
+    }
+}
---
+++ tests/Guzzle6ClientDecoratorTest.php
@@ -0,0 +1,149 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Collectors\GuzzleCollector;
+
+use GuzzleHttp\ClientInterface;
+use Miquido\RequestDataCollector\Collectors\GuzzleCollector\Guzzle6ClientDecorator;
+use Miquido\RequestDataCollector\Collectors\GuzzleCollector\GuzzleCollector;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\Argument\Token\CallbackToken;
+use Psr\Http\Message\RequestInterface;
+
+/**
+ * @coversDefaultClass \Miquido\RequestDataCollector\Collectors\GuzzleCollector\Guzzle6ClientDecorator
+ */
+class Guzzle6ClientDecoratorTest extends TestCase
+{
+    /**
+     * @var \GuzzleHttp\ClientInterface&\Prophecy\Prophecy\ObjectProphecy
+     */
+    private $clientProphecy;
+
+    /**
+     * @var \Miquido\RequestDataCollector\Collectors\GuzzleCollector\GuzzleCollector&\Prophecy\Prophecy\ObjectProphecy
+     */
+    private $guzzleCollectorProphecy;
+
+    /**
+     * @var \Miquido\RequestDataCollector\Collectors\GuzzleCollector\Guzzle6ClientDecorator
+     */
+    private $guzzle6ClientDecorator;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp(): void
+    {
+        $this->clientProphecy = $this->prophesize(ClientInterface::class);
+        $this->guzzleCollectorProphecy = $this->prophesize(GuzzleCollector::class);
+
+        /**
+         * @var \GuzzleHttp\ClientInterface                                              $clientMock
+         * @var \Miquido\RequestDataCollector\Collectors\GuzzleCollector\GuzzleCollector $guzzleCollectorMock
+         */
+        $clientMock = $this->clientProphecy->reveal();
+        $guzzleCollectorMock = $this->guzzleCollectorProphecy->reveal();
+
+        $this->guzzle6ClientDecorator = new Guzzle6ClientDecorator($clientMock, $guzzleCollectorMock);
+    }
+
+    public function testSend(): void
+    {
+        $requestDummy = $this->prepareRequestDummy();
+        $options = [];
+
+        $this->guzzleCollectorProphecy->addRequest('send', $requestDummy, $options, $this->prepareTimesArgumentAssertion())
+            ->shouldBeCalledOnce();
+
+        $this->clientProphecy->send($requestDummy, $options)
+            ->shouldBeCalledOnce();
+
+        $this->guzzle6ClientDecorator->send($requestDummy, $options);
+    }
+
+    public function testSendAsync(): void
+    {
+        $requestDummy = $this->prepareRequestDummy();
+        $options = [];
+
+        $this->guzzleCollectorProphecy->addRequest('sendAsync', $requestDummy, $options, $this->prepareTimesArgumentAssertion())
+            ->shouldBeCalledOnce();
+
+        $this->clientProphecy->sendAsync($requestDummy, $options)
+            ->shouldBeCalledOnce();
+
+        $this->guzzle6ClientDecorator->sendAsync($requestDummy, $options);
+    }
+
+    public function testRequest(): void
+    {
+        $method = 'METHOD';
+        $uri = 'uri://example.com';
+        $options = [];
+
+        $this->guzzleCollectorProphecy->addRawRequest('request', $method, $uri, $options, $this->prepareTimesArgumentAssertion())
+            ->shouldBeCalledOnce();
+
+        $this->clientProphecy->request($method, $uri, $options)
+            ->shouldBeCalledOnce();
+
+        $this->guzzle6ClientDecorator->request($method, $uri, $options);
+    }
+
+    public function testRequestAsync(): void
+    {
+        $method = 'METHOD';
+        $uri = 'uri://example.com';
+        $options = [];
+
+        $this->guzzleCollectorProphecy->addRawRequest('requestAsync', $method, $uri, $options, $this->prepareTimesArgumentAssertion())
+            ->shouldBeCalledOnce();
+
+        $this->clientProphecy->requestAsync($method, $uri, $options)
+            ->shouldBeCalledOnce();
+
+        $this->guzzle6ClientDecorator->requestAsync($method, $uri, $options);
+    }
+
+    public function testGetConfig(): void
+    {
+        $option = [];
+
+        $this->clientProphecy->getConfig($option)
+            ->shouldBeCalledOnce();
+
+        $this->guzzle6ClientDecorator->getConfig($option);
+    }
+
+    /**
+     * @return \Psr\Http\Message\RequestInterface
+     */
+    private function prepareRequestDummy(): RequestInterface
+    {
+        $requestDummy = $this->prophesize(RequestInterface::class)->reveal();
+
+        /**
+         * @var \Psr\Http\Message\RequestInterface $requestDummy
+         */
+        return $requestDummy;
+    }
+
+    /**
+     * @return \Prophecy\Argument\Token\CallbackToken
+     */
+    private function prepareTimesArgumentAssertion(): CallbackToken
+    {
+        return Argument::that(function ($value) {
+            self::assertIsArray($value);
+            self::assertArrayHasKey('started_at', $value);
+            self::assertArrayHasKey('finished_at', $value);
+            self::assertIsFloat($value['started_at']);
+            self::assertIsFloat($value['finished_at']);
+            self::assertGreaterThan($value['started_at'], $value['finished_at']);
+
+            return true;
+        });
+    }
+}
